3f41b1343badd6db4a3e95aaf1c5dcd9d9fb9814
rtt <1191623980@qq.com>
2024-12-05 01:17:25 +0800

    1205

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,23 @@ import i18n from './locals'; // 国际化
 import { useAppStoreWithOut } from '@/store/modules/app';
 import { store } from './store';
 import './styles/index.ts';
-import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import ElementPlus from 'element-plus';
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 const loading = ElLoading.service({
     lock: true,
     text: 'Loading...',
-    background: 'rgb(0, 0, 0, 0.8)',
+    background: 'rgb(0, 0, 0, 0.8)'
 });
 const app = createApp(App);
 const appStore = useAppStoreWithOut();
 
-app.use(ElementPlus, { locale: zhCn, })
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+}
+
+app.use(ElementPlus, { locale: zhCn });
 app.use(components);
 app.use(i18n);
 app.use(store);
