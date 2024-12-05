ff87a619b67d4f3112fb8bc39d7c80f32f534b01
rtt <1191623980@qq.com>
2024-12-05 20:15:23 +0800

    1205

--- src/router/home.ts
+++ src/router/home.ts
@@ -1,5 +1,5 @@
 import type { RouteRecordRaw } from 'vue-router';
-import Home from '@/views/Home.vue';
+import Home  from '@/views/Home.vue';
 
 export const routesHome: Array<RouteRecordRaw> = [
     {
